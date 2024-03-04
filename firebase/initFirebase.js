f9faa55ec1d197ffb3e541ed7d7fc39ca877dff4
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-03-04 13:25:59 +0200

    progress

---
+++ firebase/initFirebase.js
@@ -0,0 +1,17 @@
+// initFirebase.js
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const clientCredentials = {
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+}
+
+const app = initializeApp(clientCredentials)
+const db = getFirestore(app) // Initialize Firestore with the Firebase app
+
+export { app, db } // Export the initialized app and Firestore database
