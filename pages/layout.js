6f81b513fc1b800a17ac8423eb06346cff2d98ae
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-03-05 11:42:32 +0200

    progress

--- pages/layout.js
+++ pages/layout.js
@@ -1,5 +1,5 @@
 // pages/layout.js (or any component)
-import styles from '../styles/globals.module.css'
+import styles from '../styles/Home.module.css'
 
 function Layout({ children }) {
 	return <div className={styles.container}>{children}</div>
