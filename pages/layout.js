d637860809e4d6497cb635d5e4476217ba715330
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-03-05 11:11:32 +0200

    progress

--- pages/layout.js
+++ pages/layout.js
@@ -1,19 +1,8 @@
-import '../styles/globals.css' // Assuming your global styles are in 'styles/globals.css'
-import { Inter } from 'next/font/google'
-import { metadata } from './constants'
+// pages/layout.js (or any component)
+import styles from '../styles/globals.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function RootLayout({ children }) {
-	return (
-		<html lang='en'>
-			<head>
-				<link
-					rel='stylesheet'
-					href={`https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap`}
-				/>
-			</head>
-			<body className={inter.className}>{children}</body>
-		</html>
-	)
+function Layout({ children }) {
+	return <div className={styles.container}>{children}</div>
 }
+
+export default Layout
