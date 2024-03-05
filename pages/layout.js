a934342bb3c779e118760084adb1717ee11530c5
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-03-05 14:11:45 +0200

    progress

--- pages/layout.js
+++ pages/layout.js
@@ -1,8 +1,18 @@
-// pages/layout.js (or any component)
-// import styles from '.Home.module.css'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
-function Layout({ children }) {
-	return <div className={styles.container}>{children}</div>
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang='en'>
+			<Head>
+				<link
+					rel='stylesheet'
+					href={`https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap`}
+				/>
+			</Head>
+			<body className={inter.className}>{children}</body>
+		</html>
+	)
 }
-
-export default Layout
